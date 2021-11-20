dc42412b69261ce771351df62c2b2023d4205d4c
Philipp Piwo <github@piwo.space>
2021-11-20 12:01:05 +0100

    removed compatibility, compile and deploy commands

--- src/cli/commands.js
+++ src/cli/commands.js
@@ -1,12 +1,7 @@
-const compile = require('../compile/compile');
 const init = require('../init/init');
 const testConfig = require('../test/test');
 const env = require('../env/env');
-const deploy = require('../deploy/deploy');
 const config = require('../config/node-config.json');
-
-const dockerIp = config.nodeConfiguration.dockerMachineIP;
-const compatibility = require('../compatibility/compatibility');
 
 const nodeConfig = config.nodeConfiguration;
 const compilerConfig = config.compilerConfiguration;
@@ -18,15 +13,6 @@ const addInitOption = (program) => {
     .option('--update [update]', 'Update project files')
     .action(async (option) => {
       await init.run(option.update);
-    });
-};
-
-const addCompileOption = (program) => {
-  program
-    .command('compile')
-    .description(compile.deprecatedMessage)
-    .action(async (option) => {
-      await compile.run(option.path, option.compiler);
     });
 };
 
@@ -52,39 +38,10 @@ const addEnvOption = (program) => {
     });
 };
 
-const addDeployOption = (program) => {
-  program
-    .command('deploy')
-    .description('Run deploy script')
-    .option('--path [deploy path]', 'Path to deployment file', './deployment/deploy.js')
-    .option('-s --secretKey [secretKey]', 'SecretKey (privateKey) to use for deployment')
-    .option('-n --network [network]', 'Select a network defined in config/network.json', 'local')
-    .option('-c --compiler [compiler_url]', 'URL of the http compiler to use')
-    .action(async (options) => {
-      await deploy.run(options.path, options.secretKey, options.network, options.compiler);
-    });
-};
-
-const addCompatibility = (program) => {
-  program
-    .command('compatibility')
-    .description('Start env with latest versions and test the current project for compatibility')
-    .option('--nodeVersion [nodeVersion]', 'Specify node version')
-    .option('--compilerVersion [compilerVersion]', 'Specify compiler version')
-    .option('--windows', 'Start the node in windows env')
-    .option('--docker-ip [default docker machine ip]', `Set docker machine IP, default is "${dockerIp}"`, dockerIp)
-    .action(async (options) => {
-      await compatibility.run(options);
-    });
-};
-
 const initCommands = (program) => {
   addInitOption(program);
-  addCompileOption(program);
   addTestOption(program);
   addEnvOption(program);
-  addDeployOption(program);
-  addCompatibility(program);
 };
 
 module.exports = {
